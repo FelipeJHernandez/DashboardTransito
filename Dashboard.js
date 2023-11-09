523de55846e42bef4351aec9c8001f18b4677af8
Mauricio Castelazo <77642083+Mauricio-create@users.noreply.github.com>
2023-11-09 16:16:45 -0600

    Cambio_grafica

--- Dashboard.js
+++ Dashboard.js
@@ -3,37 +3,14 @@ function crearGraficaPastelHM(numHombres, numMujeres) {
         labels: ['Hombres', 'Mujeres'],
         datasets: [{
             data: [numHombres, numMujeres],
-            backgroundColor: ['#FF5733', '#3366FF'],
+            backgroundColor: ['#66ccff', '#ffccff'],
         }]
     };
 
     var opciones = {
         responsive: true,
-        maintainAspectRatio: false,
-        tooltips: {
-            callbacks: {
-                label: function(tooltipItem, data) {
-                    var dataset = data.datasets[tooltipItem.datasetIndex];
-                    var total = dataset.data.reduce(function(previousValue, currentValue) {
-                        return previousValue + currentValue;
-                    });
-                    var currentValue = dataset.data[tooltipItem.index];
-                    var percentage = Math.floor(((currentValue/total) * 100)+0.5);
-                    return data.labels[tooltipItem.index] + ': ' + percentage + '%';
-                }
-            }
-        },
-        plugins: {
-            datalabels: {
-                color: '#fff',
-                formatter: (value, ctx) => {
-                    let sum = ctx.dataset._meta[0].total;
-                    let percentage = (value * 100 / sum).toFixed(2) + "%";
-                    return percentage;
-                }
-            }
-        }
-    };
+        maintainAspectRatio: false
+    }
 
     var ctx = document.getElementById('graficaPastel').getContext('2d');
     var pastelChart = new Chart(ctx, {
